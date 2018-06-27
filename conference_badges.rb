57bb95862bccb977d5cc88491e32ae5a61a15dc8
Sushi Chanrai <sushi@pathworks.co>
2018-06-27 23:16:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speakers)
+  speakers.map { |names| "Hello, my name is #{names}." }
+end
+
+
+def assign_rooms(speakers)
+  speakers.map { |names|
+   names.each_with_index { |attendees, index|
+   "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"}}
+end
+
+
+=begin
+def assign_rooms(speakers)
+  new_array = []
+  speakers.each_with_index do |names, index| 
+  new_array << "Hello, #{names}! You'll be assigned to room #{index + 1}!"
+  end
+return new_array
+end
+=end
+
+
+def printer(speakers)
+  batch_badge_creator(speakers).each { |badges| puts badges }
+  assign_rooms(speakers).each { |rooms| puts rooms}
+end
